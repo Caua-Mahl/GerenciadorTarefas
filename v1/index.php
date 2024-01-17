9404b0e63ede16ae19d1f5ed208f8f9617a1a823
Cauã Mahl <cauamahl@yahoo.com.br>
2024-01-17 11:56:10 -0300

    desenvolvendo v1

---
+++ v1/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gerenciador de Tarefas</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="header">
+            <h1>Gerenciador de tarefas</h1>
+
+        </div>
+        <div class="form">
+            <form action="" method="get">
+                <input type="text" name="nome_tarefa" placeholder="Nome da Tarefa">
+                <button type="submit">Cadastrar</button>
+
+
+            </form>
+
+        </div>
+        <div class="separator">
+
+
+        </div>
+        <div class="lista-tarefas">
+
+            <ul>
+                <li>tarefa 1</li>
+                <li>tarefa 2</li>
+                <li>tarefa 3</li>
+
+            </ul>
+
+        </div>
+        <div class="footer">
+
+
+
+        </div>
+
+
+
+
+    </div>
+
+    <?php
+
+
+    ?>
+</body>
+
+</html>
